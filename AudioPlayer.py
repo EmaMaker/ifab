2c06882e8e001a08cdb814ce80fdfee4db0fa662
EmaMaker <emamaker@tutanota.com>
2025-03-29 12:40:17 +0100

    Initial commit
    notes + alfy's python lib

---
+++ AudioPlayer.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+import queue
+import threading
+import wave
+
+import numpy as np
+import sounddevice as sd
+from piper.voice import PiperVoice
+
+
+class AudioPlayer:
+    def __init__(self, voice_model_path):
+        self.voice = PiperVoice.load(voice_model_path)
+        self.stream = None
+        self.queue = queue.Queue()
+        self.thread = threading.Thread(target=self._play_audio)
+        self.thread.daemon = True
+        self.thread.start()
+        self.dType = np.int16
+        self.blockItemSize = 4096
+        self.silence_sound = np.zeros(int(self.voice.config.sample_rate * 0.3), dtype=self.dType)
+
+    def _ensure_stream_open(self):
+        if self.stream is None or not self.stream.active:
+            self.stream = sd.OutputStream(
+                samplerate=self.voice.config.sample_rate,
+                channels=1,
+                dtype=self.dType,
+                blocksize=self.blockItemSize,
+                latency='low'
+            )
+            self.stream.start()
+
+    def _play_audio(self):
+        while True:
+            audio_data = self.queue.get()
+            if audio_data is None:
+                break
+            self._ensure_stream_open()
+            self.stream.write(audio_data)
+        if self.stream:
+            self.stream.stop()
+            self.stream.close()
+            self.stream = None
+
+    def play_wav_from_memory(self, wav_data):
+        index = 0
+        toSend = len(wav_data) - 1 // np.dtype(self.dType).itemsize
+        while toSend > 0:
+            int_data = np.frombuffer(wav_data, dtype=np.int16, count=min(self.blockItemSize, toSend // np.dtype(self.dType).itemsize), offset=index)
+            self.queue.put(int_data)
+            index += self.blockItemSize * np.dtype(self.dType).itemsize
+            toSend -= self.blockItemSize * np.dtype(self.dType).itemsize
+
+    def play_text(self, text):
+        for audio_bytes in self.voice.synthesize_stream_raw(text):
+            int_data = np.frombuffer(audio_bytes, dtype=np.int16)
+            self.queue.put(int_data)
+
+    def is_playing(self):
+        return self.stream is not None and self.stream.active
+
+    def waitEndBuffer(self):
+        self.queue.put(self.silence_sound)
+        self.queue.put(None)
+        self.thread.join()
+        self.thread = threading.Thread(target=self._play_audio)
+        self.thread.daemon = True
+        self.thread.start()
+
+
+def open_wave(wave_file):
+    with wave.open(wave_file, 'rb') as wf:
+        wav_data = wf.readframes(wf.getnframes())
+    return wav_data
---
+++ notes.md
@@ -0,0 +1,67 @@
+Il nostro sito redbauble.dev non è indicizzato su motori di ricerca -> non posso usare il sito come knoledge base perché fa le ricerche su Bing e poi filtra per sito
+
+Soluzione: `datasets-to-kb.sh` prende i file markdown del dataset di wikidocs e li trasforma in file markdown con nome directory.md, da caricare su Copilot Studio come knowledge base.
+Lo script richiede accedere al volume docker (quindi root). Wikidocs dà problemi quando il volume fa rifarimento a utente non root per qualche motivo.
+
+# Usare bot con API python (Direct Line 3.0)
+
+Il bot deve essere stato pubblicato https://learn.microsoft.com/en-us/microsoft-copilot-studio/publication-connect-bot-to-custom-application
+
+https://learn.microsoft.com/en-us/microsoft-copilot-studio/publication-connect-bot-to-web-channels?tabs=preview
+
+
+https://learn.microsoft.com/en-us/azure/bot-service/rest-api/bot-framework-rest-direct-line-3-0-api-reference?view=azure-bot-service-4.0
+
+Il bot è regionale europe e non ho capito perché
+
+
+## Aprire conversazione
+
+POST https://directline.botframework.com/v3/directline/conversations
+
+```
+import requests
+
+url_convopen = "https://europe.directline.botframework.com/v3/directline/conversations"
+
+header = {"Authorization": 'Bearer Ec99xFUkF1i7cR8m5TLtPokIlKXvLNdCxIYyDsraweBmf2zltwUZJQQJ99BCACi5YpzAArohAAABAZBSECEz.IpVjYOfmWMOQOHYGdH4G16pGKUArN1pEpAGJebfBjSrKI71E6ZhDJQQJ99BCACi5YpzAArohAAABAZBSMCrh'}
+
+r = requests.post(url_convopen, headers=header)
+```
+
+The part after Bearer is the SECRET from Agent>Settings>Security>Secret
+
+
+## Mandare attività
+
+```
+body = """{
+    "locale": "en-EN",
+    "type": "message",
+    "from": {
+        "id": "user1"
+    },
+    "text": "hello"
+}"""
+
+
+header = {"Authorization": 'Bearer Ec99xFUkF1i7cR8m5TLtPokIlKXvLNdCxIYyDsraweBmf2zltwUZJQQJ99BCACi5YpzAArohAAABAZBSECEz.IpVjYOfmWMOQOHYGdH4G16pGKUArN1pEpAGJebfBjSrKI71E6ZhDJQQJ99BCACi5YpzAArohAAABAZBSMCrh', 'Content-Type': 'application/json'}
+
+url_act = 'https://europe.directline.botframework.com/v3/directline/conversations/CONV-ID-FROM-CONVERSATION-OPEN/activities'
+"
+
+r = requests.post(url_act, headers=header, data=body)
+```
+
+The important part is data, instead of params
+
+
+## Ricevere attività
+Facilmente, via websockets con lo stream url contenuto nella risposta dell'apertura della conversazione
+
+```python3 -m websockets URL```
+
+## TTS Alfano
+python test-scripts/tts-compare.py --model ./tts-model/it_IT-paola-medium.onnx
+
+
