5f18bc94d242f09b43622c3db2ad3b8bd4e385a8
EmanueleAlfano <emanuele.alfano@enel.com>
2025-04-19 17:43:17 +0200

    aruco_quadrilateral_transformer.py modificato find_qudrilateral per printare a prescindere quello che vede, ed aiutare il debug

--- Camera-SubSystem/arucoRead.py
+++ Camera-SubSystem/arucoRead.py
@@ -1,48 +1,50 @@
 import cv2
 from cv2_enumerate_cameras import enumerate_cameras
 
-for camera_info in enumerate_cameras(cv2.CAP_V4L2):
-    print(f'{camera_info.index}: {camera_info.name}')
+def generateCamera(index:int = 0) -> tuple:
+    """
+    Detect the index of the camera.
+    :return: Camera index
+    """
+    # Open the default camera
+    cam = cv2.VideoCapture(index)
+    # Get the default frame width and height
+    frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    return cam, frame_width, frame_height
 
-# Open the default camera
-cam = cv2.VideoCapture(0)
 
-# Get the default frame width and height
-frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+if __name__ == '__main__':
 
-# # Define the codec and create VideoWriter object
-# fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-# out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+    cam, frame_width, frame_height = generateCamera(0)
+    while True:
+        ret, frame = cam.read()
 
-while True:
-    ret, frame = cam.read()
+        # Convert the image to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+        parameters = cv2.aruco.DetectorParameters()
 
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
-    parameters = cv2.aruco.DetectorParameters()
+        # Create the ArUco detector
+        detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
+        # Detect the markers
+        corners, ids, rejected = detector.detectMarkers(gray)
+        # Print the detected markers
+        if ids is not None:
+            cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+            for i in range(len(ids)):
+                # Stampa le coordinate dei marker
+                pointList = str(corners[i]).replace('\n','')
+                print(f'Marker ID: {ids[i][0]}\tCoordinates: {pointList}')
+            print("-"*60)
 
-    # Create the ArUco detector
-    detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
-    # Detect the markers
-    corners, ids, rejected = detector.detectMarkers(gray)
-    # Print the detected markers
-    if ids is not None:
-        cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-        for i in range(len(ids)):
-            # Stampa le coordinate dei marker
-            pointList = str(corners[i]).replace('\n','')
-            print(f'Marker ID: {ids[i][0]}\tCoordinates: {pointList}')
-        print("-"*60)
+        # Display the captured frame
+        cv2.imshow('Camera', frame)
 
-    # Display the captured frame
-    cv2.imshow('Camera', frame)
+        # Press 'q' to exit the loop
+        if cv2.waitKey(1) == ord('q'):
+            break
 
-    # Press 'q' to exit the loop
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-# Release the capture and writer objects
-cam.release()
-cv2.destroyAllWindows()
+    # Release the capture and writer objects
+    cam.release()
+    cv2.destroyAllWindows()
