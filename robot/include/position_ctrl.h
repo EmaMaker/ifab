46004b338729340bd917506f0c50d87679280e0f
EmaMaker <emamaker@tutanota.com>
2025-05-02 18:19:57 +0200

    pos_ctrl: [IMPORTANT] further fine tuning, FSM fixes, refactor
    - slight refactor
    	- cleanup redundant code
    	- constrains on v, omega for both position and orientation controller
    - histeresis on camera data: do not accept new values if too close to old ones
    	- avoids oscillations
    	- robust to udp packets arriving out-of-order
    	- add condition to not move when near target, only re-orient
    - position controller FSM fixes
    	- add transitions from IDLE to POS, ORIENT_FINAL: fixes the robot sometimes remaining stuck far from the target

--- robot/include/position_ctrl.h
+++ robot/include/position_ctrl.h
@@ -4,28 +4,34 @@
 #include "odo_loc.h"
 
 #define CTRL_PHASE_INIT_POSITION -1
-#define CTRL_PHASE_INIT_ORIENT_FINAL -2
-#define CTRL_PHASE_POSITION 0
-#define CTRL_PHASE_IDLE -10
-#define CTRL_PHASE_ORIENT_FINAL 1
+// #define CTRL_PHASE_INIT_ORIENT_START -2
+#define CTRL_PHASE_INIT_ORIENT_FINAL -3
+#define CTRL_PHASE_POSITION 1
+#define CTRL_PHASE_IDLE 0
+// #define CTRL_PHASE_ORIENT_START 2
+#define CTRL_PHASE_ORIENT_FINAL 3
 
-#define KP_X 1.25
+#define KP_X 0.65
 #define KI_X 0
 #define KD_X 0
-#define KP_Y 1.25
+#define KP_Y 0.65
 #define KI_Y 0
 #define KD_Y 0
-#define KP_ORIENT 0.65
+#define KP_ORIENT 1.25
 #define KI_ORIENT 0
 #define KD_ORIENT 0
-constexpr float V_MAX = 0.3;
-constexpr float A_MAX = 0.2;
+
+constexpr float MAX_ANGULAR_SPEED = 3; // rad/s
+constexpr float V_MAX = 0.35;
+constexpr float A_MAX = 0.1;
 constexpr float Ts = V_MAX / A_MAX;
 
 constexpr float B_FROM_CENTER = 0.02;
 
-constexpr float MAX_ANGULAR_SPEED = 0.8; // rad/s
-constexpr float VALID_DIST_FROM_TARGET = 0.075;
+constexpr float VALID_DIST_FROM_TARGET_POS = 0.05;
+constexpr float VALID_DIST_FROM_TARGET_ORIENT = radians(5);
+constexpr float CAMERA_HISTERESIS_POSITION = 0.025;
+constexpr float CAMERA_HISTERESIS_ORIENT = radians(5);
 
 void init_position_ctrl(void);
 void update_position_ctrl(void);
@@ -42,6 +48,7 @@ double dst_sq(position_t, position_t);
 double err_orient(position_t, position_t);
 
 double angle_diff(double, double);
+double angle_diff_min(double, double);
 
 
 #endif
--- robot/src/position_ctrl.cpp
+++ robot/src/position_ctrl.cpp
@@ -39,9 +39,9 @@ void init_position_ctrl(void){
     ctrl_orient.SetSampleTimeUs(sample_time_position_millis*1e3);
 
     // motors are rated at about 200RPM -> 20 rad/s
-    ctrl_x.SetOutputLimits(-5, 5);
-    ctrl_y.SetOutputLimits(-5, 5);
-    ctrl_orient.SetOutputLimits(-MAX_ANGULAR_SPEED, MAX_ANGULAR_SPEED);  
+    ctrl_x.SetOutputLimits(-25*ODO_WHEEL_RADIUS, 25*ODO_WHEEL_RADIUS);
+    ctrl_y.SetOutputLimits(-25*ODO_WHEEL_RADIUS, 25*ODO_WHEEL_RADIUS);
+    ctrl_orient.SetOutputLimits(-25*ODO_WHEEL_RADIUS, 25*ODO_WHEEL_RADIUS);
 
     timer_position_update = 0;
     timer_position_ctrl = 0;
@@ -75,10 +75,16 @@ void update_position_ctrl(){
       angspd[0] = 0;
       angspd[1] = 0;
       // Serial.println("IDLE");
+
+      // Position control if far from target
+      if(dst(position_robot, position_fin) > VALID_DIST_FROM_TARGET_POS) ctrl_phase = CTRL_PHASE_INIT_POSITION;
+      // Orientation control if in position but not pointing target
+      else if(abs(angle_diff_min(position_robot.theta, position_fin.theta) > VALID_DIST_FROM_TARGET_ORIENT)) ctrl_phase = CTRL_PHASE_INIT_ORIENT_FINAL;
   }
   if(ctrl_phase == CTRL_PHASE_INIT_POSITION){
     ctrl_x.Initialize();
     ctrl_y.Initialize();
+    ctrl_orient.SetMode(ctrl_orient.Control::manual);
     ctrl_x.SetMode(ctrl_x.Control::automatic);
     ctrl_y.SetMode(ctrl_y.Control::automatic);
 
@@ -91,31 +97,32 @@ void update_position_ctrl(){
     ctrl_phase = CTRL_PHASE_POSITION;  
   }
   if(ctrl_phase == CTRL_PHASE_POSITION){
-    if(dst(position_robot, position_fin) <= VALID_DIST_FROM_TARGET){
+    ctrl_orient.SetMode(ctrl_orient.Control::manual);
+
+    if(dst(position_robot, position_fin) <= VALID_DIST_FROM_TARGET_POS){
       ctrl_x.SetMode(ctrl_x.Control::manual);
       ctrl_y.SetMode(ctrl_y.Control::manual);
 
       ctrl_phase = CTRL_PHASE_INIT_ORIENT_FINAL;
     }else controller_position(angspd);
   }
-      // Serial.println("POS");
+
   if (ctrl_phase == CTRL_PHASE_INIT_ORIENT_FINAL){
     ctrl_orient.Initialize();
+    ctrl_x.SetMode(ctrl_x.Control::manual);
+    ctrl_y.SetMode(ctrl_y.Control::manual);
     ctrl_orient.SetMode(ctrl_orient.Control::automatic);
-
     ctrl_phase = CTRL_PHASE_ORIENT_FINAL;
-    // Serial.println("INIT ORIENT");
   }
+ 
   if (ctrl_phase == CTRL_PHASE_ORIENT_FINAL){
-      // Serial.println("ORIENT");
-      
-    double d1 = angle_diff(position_robot.theta, position_fin.theta);
-    double d2 = TWO_PI - d1;
-    double d = -min(d1, d2);
-    
+    ctrl_x.SetMode(ctrl_x.Control::manual);
+    ctrl_y.SetMode(ctrl_y.Control::manual);
+
+    double d = -angle_diff_min(position_robot.theta, position_fin.theta);
     pos_orient = d;
     setpoint_orient = 0;
-    if(abs(d) <= radians(10)){
+    if(abs(d) <= VALID_DIST_FROM_TARGET_ORIENT){
       ctrl_orient.SetMode(ctrl_orient.Control::manual);
       ctrl_phase = CTRL_PHASE_IDLE;
 
@@ -132,18 +139,18 @@ void update_position_ctrl(){
 
   // I think UDP as a buffer and calls are blocking if the buffer is full
   // ToDo: maybe offload fast debugging to second core?
-  // if (timer_debug >= 500){
-  //   String m = String(micros());
-  //   udp_send_debug_string("x", m, String(position_robot.x), true);
-  //   udp_send_debug_string("y", m, String(position_robot.y), true);
-  //   udp_send_debug_string("theta", m, String(position_robot.theta), true);
-  //   udp_send_debug_string("target_x", m, String(position_fin.x), true);
-  //   udp_send_debug_string("target_y", m, String(position_fin.y), true);
-  //   udp_send_debug_string("target_theta", m, String(position_fin.theta), true);
-  //   udp_send_debug_string("CTRL_PHASE", m, String(ctrl_phase), true);
+  if (timer_debug >= 500){
+    String m = String(micros());
+    udp_send_debug_string("x", m, String(position_robot.x), true);
+    udp_send_debug_string("y", m, String(position_robot.y), true);
+    udp_send_debug_string("theta", m, String(position_robot.theta), true);
+    udp_send_debug_string("target_x", m, String(position_fin.x), true);
+    udp_send_debug_string("target_y", m, String(position_fin.y), true);
+    udp_send_debug_string("target_theta", m, String(position_fin.theta), true);
+    udp_send_debug_string("CTRL_PHASE", m, String(ctrl_phase), true);
 
-  //   timer_debug = 0;
-  // }
+    timer_debug = 0;
+  }
 }
 
 void controller_position(double output[]){
@@ -161,20 +168,18 @@ void controller_position(double output[]){
   float T = (L*A_MAX + V_MAX*V_MAX)/(V_MAX*A_MAX);
 
   // A_MAX = V_MAX / Ts;
-  if (tf<= Ts && tf>= 0){
-
+  if (tf <= Ts){
     s_dot = A_MAX*(tf - traj_start_time)/L;
     s = A_MAX * tf*tf / (2*L);
   }
-  else if (tf<= T -Ts && tf> Ts){
+  else if (tf <= T - Ts && tf > Ts){
     s_dot = A_MAX * Ts / L;
     s = A_MAX * Ts * (tf- Ts)/L + A_MAX * Ts * Ts / (2*L);
   }
-  else if (tf<= T && tf> T - Ts){
+  else if (tf > T - Ts){
     s_dot = A_MAX*(T - tf)/L;
     s = A_MAX*T * (tf- T + Ts)/L - A_MAX * ((tf*tf - (T-Ts)*(T-Ts)) / (2*L)) + A_MAX * Ts *(T-Ts - Ts)/L + A_MAX * Ts * Ts / (2*L);
   }
-  else s_dot = 0;
 
   setpoint_x = position_init.x + s * (position_fin.x - position_init.x); //x_des
   setpoint_y = position_init.y + s * (position_fin.y - position_init.y); //y_des
@@ -191,38 +196,31 @@ void controller_position(double output[]){
 
   decouple_t(out_t, in_t);
 
-  // Saturate only the angular speed (omega)
+  // Saturation on v, omega
+  out_t[0] = constrain(out_t[0], -V_MAX, V_MAX);
+  out_t[1] = constrain(out_t[1], -MAX_ANGULAR_SPEED, MAX_ANGULAR_SPEED);
 
-  if (out_t[1] > MAX_ANGULAR_SPEED) {
-    out_t[1] = MAX_ANGULAR_SPEED;
-  }
-
-  else if (out_t[1] < -MAX_ANGULAR_SPEED) {
-    out_t[1] = -MAX_ANGULAR_SPEED;
-  }
-  
   decouple_w(output, out_t);
 }
 
 void controller_orient(double output[]){
   ctrl_orient.Compute();
   double inw[] = {0, output_orient};
+
+  // Saturate omega only
+  inw[1] = constrain(inw[1], -MAX_ANGULAR_SPEED, MAX_ANGULAR_SPEED);
+
   decouple_w(output, inw);
 }
 
 void decouple_t(double output[], double input[]){
-    constexpr double d = ODO_DISTANCE_BETWEEN_WHEELS;
-    constexpr double r = ODO_WHEEL_RADIUS;
-    constexpr double b = B_FROM_CENTER;
-    constexpr double det = 2*b*r;
+  double t_11 = cos(position_robot.theta);
+  double t_12 = sin(position_robot.theta);
+  double t_21 = -sin(position_robot.theta)/B_FROM_CENTER;
+  double t_22 = cos(position_robot.theta)/B_FROM_CENTER;
 
-    double t_11 = cos(position_robot.theta);
-    double t_12 = sin(position_robot.theta);
-    double t_21 = -sin(position_robot.theta)/b;
-    double t_22 = cos(position_robot.theta)/b;
-
-    output[0] = t_11 * input[0] + t_12*input[1];
-    output[1] = t_21 * input[0] + t_22*input[1];
+  output[0] = t_11 * input[0] + t_12*input[1];
+  output[1] = t_21 * input[0] + t_22*input[1];
 }
 
 void decouple_w(double output[], double input[]){
@@ -231,25 +229,40 @@ void decouple_w(double output[], double input[]){
   constexpr double b = B_FROM_CENTER;
   constexpr double det = 2*b*r;
 
-  double w_11 = 1/r;
-  double w_12 = d/(2*r);
-  double w_21 = 1/r;
-  double w_22 = -d/(2*r);
+  constexpr double w_11 = 1/r;
+  constexpr double w_12 = d/(2*r);
+  constexpr double w_21 = 1/r;
+  constexpr double w_22 = -d/(2*r);
 
   output[0] = w_11*input[0] + w_12*input[1];
   output[1] = w_21*input[0] + w_22*input[1];
 }
 
 void set_robot_position(position_t p){
-  position_robot = p;
-  position_robot.tk = 0;
+  // little bit of histeresis, to avoid noise from camera interfering with control
+  if(dst(position_robot, p) >= CAMERA_HISTERESIS_POSITION){
+    position_robot = p;
+    position_robot.tk = 0;
+  }
 }
 
 void set_desired_position(position_t goal){
-  position_init = position_robot;
-  position_fin = goal;
-
-  if (ctrl_phase != CTRL_PHASE_POSITION) ctrl_phase = CTRL_PHASE_INIT_POSITION;
+  // little bit of histeresis, to avoid noise from camera interfering with control
+  if(dst(position_fin, goal) >= CAMERA_HISTERESIS_POSITION){
+    position_init = position_robot;
+    position_fin = goal;
+    
+    // Re-init position ctrl if not already going towards target
+    if (ctrl_phase != CTRL_PHASE_POSITION) ctrl_phase = CTRL_PHASE_INIT_POSITION;
+  }
+  // same histeresis, but on the orientation
+  else if(abs(angle_diff_min(position_fin.theta, goal.theta)) >= CAMERA_HISTERESIS_ORIENT){
+    position_init = position_robot;
+    position_fin = goal;
+    
+    // Re-init position ctrl if not already going towards target
+    if(ctrl_phase != CTRL_PHASE_ORIENT_FINAL) ctrl_phase = CTRL_PHASE_INIT_ORIENT_FINAL;
+  }
 }
 
 double dst(position_t p1, position_t p2){
@@ -274,4 +287,21 @@ double angle_diff(double a1, double a2){
   double cprod_z = -n1y*n2x + n1x*n2y;
   double sign = cprod_z >= 0 ? 1 : -1;
   return sign * acos(sprod);
+}
+
+double angle_diff_min(double a1, double a2){
+  double n1x = cos(a1);
+  double n1y = sin(a1);
+  double n1z = 0;
+  double n2x = cos(a2);
+  double n2y = sin(a2);
+  double n2z = 0;
+
+  double sprod = n1x*n2x + n1y*n2y + n1z*n2z;
+  double cprod_z = -n1y*n2x + n1x*n2y;
+  double sign = cprod_z >= 0 ? 1 : -1;
+
+  double m1 = sign * acos(sprod);
+  double m2 = TWO_PI - m1;
+  return min(m1, m2);
 }
